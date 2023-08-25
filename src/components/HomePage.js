ce1ccc8353ccd5bf5ac517443aa04cc455ba3dd4
ishimwepati <wazacode@gmail.com>
2023-08-25 23:33:09 +0300

    Delete working

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,25 +1,26 @@
 import React, { useState } from 'react';
 import { books } from './components/bookData';
 import BookList from './components/BookList';
-import CreateBookForm from './components/CreateBookForm'; // Import the CreateBookForm component
+import CreateBookForm from './components/CreateBookForm';
 
 function HomePage() {
-  
   const [bookList, setBookList] = useState(books);
 
-  
+  const handleDeleteBook = (bookId) => {
+    const updatedBooks = bookList.filter((book) => book.id !== bookId);
+    setBookList(updatedBooks);
+  };
+
   const handleAddBook = (newBook) => {
-   
     newBook.id = bookList.length + 1;
-   
     setBookList([...bookList, newBook]);
   };
 
   return (
     <div>
       <h1>Waza BookStore</h1>
-      <BookList books={bookList} /> 
-      <CreateBookForm onAddBook={handleAddBook} /> 
+      <BookList books={bookList} onDelete={handleDeleteBook} /> {/* Pass onDelete prop */}
+      <CreateBookForm onAddBook={handleAddBook} />
     </div>
   );
 }
