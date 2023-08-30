00704343fc9af4815e9be19a5dc67a39fa5e38f5
ishimwepati <wazacode@gmail.com>
2023-08-30 22:14:30 +0300

    ReadMe file updated

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [], // Initial state as an empty array
+  books: [],
 };
 
 const booksSlice = createSlice({
@@ -9,18 +9,14 @@ const booksSlice = createSlice({
   initialState,
   reducers: {
     addBook: (state, action) => {
-      // Add the new book to the books array
       state.books.push(action.payload);
     },
     removeBook: (state, action) => {
-      // Remove a book by its ID from the books array
       state.books = state.books.filter((book) => book.id !== action.payload);
     },
   },
 });
 
-// Export the actions
 export const { addBook, removeBook } = booksSlice.actions;
 
-// Export the reducer
 export default booksSlice.reducer;
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,24 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  categories: [], // Initial state as an empty array
+  categories: [],
 };
 
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
   reducers: {
-    // Define any reducer actions for categories here
-    // In your case, you mentioned checking status and returning "Under construction"
+
     checkStatus: (state) => {
-      // Set the categories array to ["Under construction"]
       state.categories = ['Under construction'];
     },
   },
 });
 
-// Export the actions
 export const { checkStatus } = categoriesSlice.actions;
 
-// Export the reducer
 export default categoriesSlice.reducer;
